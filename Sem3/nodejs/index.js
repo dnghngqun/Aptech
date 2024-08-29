c1aaa8283e7a80a4f9123aecf0c7d5da8830e7c4
hongquan <danghongquanvtc1111@gmail.com>
2024-08-29 09:35:57 +0700

    up

--- Sem3/nodejs/index.js
+++ Sem3/nodejs/index.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Item = require("./models/Item")
+const Item = require("./models/Item");
+
+const app = express();
+const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/ecommerceDB');
